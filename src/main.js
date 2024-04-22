4c8a076071e345b810c6b6b7673cc197458a2884
Mariusz <mariusz.c008@protonmail.com>
2024-04-22 21:06:54 +0200

    checking new day

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ const counterValue = document.querySelector('.counter--js');
 
 let glasses = 0;
 
-const key = new Date().toISOString().slice(0, 10);
+let key = new Date().toLocaleString().slice(0, 10);
+
+setInterval(() => {
+    if (key !== new Date().toLocaleString().slice(0, 10)); {
+    key = new Date().toLocaleString().slice(0, 10)
+    setCounterValue(0)
+    glasses=0;
+    }
+}, 1000*60)
 
 if (localStorage.getItem(key)) {
     glasses = parseInt(localStorage.getItem(key));}
